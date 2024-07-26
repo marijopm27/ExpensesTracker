484453d6f93f6089eb27ea64378155b003c21c7c
María José Porras <79280750+marijopm27@users.noreply.github.com>
2024-07-26 10:30:37 -0600

    Responsive Navbar Completed

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const toggleMenu = document.querySelector('.toggle-menu');
+    const navbar = document.querySelector('.navbar');
 
+    toggleMenu.addEventListener('click', function() {
+        navbar.classList.toggle('active');
+    });
+});
